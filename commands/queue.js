a5596b531c07c48e60f8f48f427c702c70de94ab
OlliePCK <ollieker3108@gmail.com>
2021-01-05 07:24:41 +1100

    Added command to show music queue

--- commands/queue.js
+++ commands/queue.js
@@ -1,5 +1,19 @@
-module.exports.run = (message, args, client, queue, searcher) => {
-	console.log('Sim queue');
+module.exports.run = (message, args, client, queue) => {
+	const serverQueue = queue.get(message.guild.id);
+	if (!serverQueue) {
+		return message.channel.send('There is no music currently playing!');
+	}
+	if(message.member.voice.channel != message.guild.me.voice.channel) {
+		return message.channel.send('You need to join the voice chat first!');
+	}
+	const nowPlaying = serverQueue.songs[0];
+	let queueMessage = `Now playing: ${nowPlaying.title}\n-----------------------\n`;
+
+	for (let i = 1; i < serverQueue.songs.length; i++) {
+		queueMessage += `${i}. ${serverQueue.songs[i].title}\n`;
+	}
+
+	message.channel.send('```' + queueMessage + 'Requested by: ' + message.author.username + '```');
 };
 
 module.exports.config = {
