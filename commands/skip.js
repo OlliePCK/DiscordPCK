8e2565362828b17b9c8e5fe70e93fa170402c17c
OlliePCK <ollieker3108@gmail.com>
2021-01-07 18:48:44 +1100

    Change order of if statement

--- commands/skip.js
+++ commands/skip.js
@@ -1,10 +1,10 @@
 module.exports.run = (message, args, client, queue) => {
 	const serverQueue = queue.get(message.guild.id);
+	if (!serverQueue || serverQueue.connection.dispatcher == null) {
+		return message.channel.send('There is nothing to skip!');
+	}
 	if(message.member.voice.channel != message.guild.me.voice.channel) {
 		return message.channel.send('You need to join the voice chat and play a song first!');
-	}
-	if(!serverQueue || serverQueue.connection.dispatcher == null) {
-		return message.channel.send('There is nothing to skip!');
 	}
 	serverQueue.connection.dispatcher.end();
 	return message.channel.send('The song has been skipped!');
